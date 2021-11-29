49f9a2887f92b67046671a7e0d85ae870e729a68
jordi <jordigraurios@gmail.com>
2021-11-29 15:47:47 +0100

    changes

--- Assets/Scripts/Game/Gameplay/Controllers/MouseFlightController.cs
+++ Assets/Scripts/Game/Gameplay/Controllers/MouseFlightController.cs
@@ -79,6 +79,13 @@ namespace Gameplay.controllers
             }
         }
 
+        // Use this for initialization
+        void Start()
+        {
+            //Set Cursor to not be visible
+            Cursor.visible = false;
+        }
+
         private void Awake()
         {
             if (aircraft == null)
