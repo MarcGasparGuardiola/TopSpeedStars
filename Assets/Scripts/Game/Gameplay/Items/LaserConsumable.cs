e3251edc43aa70222231a6d5f00292ac56026c8e
Jordion69 <jortiznavas0@gmail.com>
2021-12-03 18:20:56 +0100

    Crean raig laser

--- Assets/Scripts/Game/Gameplay/Items/LaserConsumable.cs
+++ Assets/Scripts/Game/Gameplay/Items/LaserConsumable.cs
@@ -9,6 +9,58 @@ namespace Gameplay.actors
 
         private string NAME = "LaserConsumable";
         public LaserConsumable()
+
+            //Codi copiat 
+        public ParticleSystem laserStartParticles;
+        public ParticleSystem laserEndParticiples;
+        public float lineLength = 10f;
+        public LineRenderer;
+            private bool sfxIsPlaying = false;
+        private bool startParticlesPlaying = false;
+        private bool endParticlesPlaying = false;
+        private RaycastHit2D hit;
+
+        //Codi copiat Start is called before the first frame update
+
+        void Start()
+        {
+            LineRenderer = GetComponent<LineRenderer>();
+            AudioSource = GetComponent<AudioSource>();
+        }
+
+        //Codi copiat Update is called once per frame
+
+        void Update ()
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                if(startParticlesPlaying == false)
+                {
+                    startParticlesPlaying = true;
+                    laserStartParticles.Play(true);
+                }
+                laserStartParticles.gameObject.transform.position = transform.position;
+                line.enabled = true;
+                if(sfxIsPlaying == false)
+                {
+                    sfxIsPlaying = true;
+                    audioSource.Play();
+                }
+                hit = Physics2D.Raycast(transform.position, Vector2.right, lineLength, layerMask);
+                if (hit)
+                {
+                    if(endParticlesPlaying == false)
+                    {
+                        endParticlesPlaying = true;
+                        laserEndParticiples.Play(true);
+                    }
+                    float distance = ((Vector2)hit.point)
+                }
+
+            }
+        }
+
+
         
 
         {
@@ -22,12 +74,12 @@ namespace Gameplay.actors
         {
             RaycastHit hit;
 
-            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0f))
+            if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 100.0f))
                 print("Found an object - distance: " + hit.distance);
         }
-        void FixedUpdate()
-        {
+        
+        
            
-        }
+        
     }
 }
