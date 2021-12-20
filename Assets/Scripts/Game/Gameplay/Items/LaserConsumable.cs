8fd0e13c4f42fdc666608985c52e0306252f53e6
jordi <jordigraurios@gmail.com>
2021-12-20 15:26:19 +0100

    test data limit

--- Assets/Scripts/Game/Gameplay/Items/LaserConsumable.cs
+++ Assets/Scripts/Game/Gameplay/Items/LaserConsumable.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
 namespace Gameplay.actors
 {
     public class LaserConsumable : Consumable
@@ -83,3 +83,4 @@ namespace Gameplay.actors
         
     }
 }
+*/
