16560b930ba036b2f9fd9e873adba2a724062779
jordi <jordigraurios@gmail.com>
2022-04-17 19:27:20 +0200

    bug solve pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -42,6 +42,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu() 
     {
         Time.timeScale = 1f;
+        gameIsPaused = false;
         SceneManager.LoadScene("ResultsList");
     }
     public void QuitGame() 
